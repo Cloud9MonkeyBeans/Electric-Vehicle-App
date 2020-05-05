7310b9624ac1d668d27eee3c9de93806f75ce3ad
David <David@DESKTOP-QFURPFT>
2020-05-05 15:36:34 -0700

    initial commit

---
+++ Electric Vehicle App.Tests/UnitTest1.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace Electric_Vehicle_App.Tests
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+        }
+    }
+}
